07797e80dd006a5dbebacd491a463d119998e3ee
shijiuchen <1013851172@qq.com>
2019-04-26 16:45:22 +0800

    revise index.js file

--- naivecoin-wallet/config/index.js
+++ naivecoin-wallet/config/index.js
@@ -18,16 +18,16 @@ module.exports = {
         pathRewrite: {
           '^/api': ''
         }
+      },
+      '/appi': {
+        target: 'http://192.168.1.56:3001',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/appi': ''
+        }
       }
     },
-    '/appi': {
-      target: 'http://192.168.1.56:3001',
-      changeOrigin: true,
-      pathRewrite: {
-        '^/appi': ''
-      }
-    }
-  },
+
 
     // Various Dev Server settings
     host: '0.0.0.0', // can be overwritten by process.env.HOST
